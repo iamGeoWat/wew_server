8f8b45add259ad4534c342cdf587dd3dd7a0f34f
geowat <ldsxxs@163.com>
2018-06-14 11:16:02 +0800

    fix transfer crisis

--- routes/sendEther.js
+++ routes/sendEther.js
@@ -13,24 +13,13 @@ var json = {
   err: undefined
 };
 
-function setWeb3Provider(keystore) {
-  var web3Provider = new HookedWeb3Provider({
-    host: "https://ropsten.infura.io/KTxDHirVMJjBc0oLkjR2",
-    transaction_signer: keystore
-  });
-  web3.setProvider(web3Provider);
-}
+// function setWeb3Provider(keystore) {
+//
+// }
 
-function genAddress(password) {
-  console.log(password)
-  var numAddr = 1;
-  keystore.keyFromPassword(password, function(err, pwDerivedKey) {
-    console.log(password)
-    keystore.generateNewAddress(pwDerivedKey, numAddr);
-    var serverGenAddr = keystore.getAddresses();
-    console.log(serverGenAddr);
-  })
-}
+// function genAddress(password) {
+//
+// }
 
 
 var express = require('express');
@@ -43,6 +32,7 @@ router.post('/', function(req, res, next) {
   var fromAddr = req.body.fromAddr;
   var toAddr = req.body.toAddr;
   var valueInEth = req.body.valueInEth;
+  var valueInWei = parseFloat(valueInEth)*1.0e18;
   var ifCustom = req.body.ifCustom;
   
   var password = req.body.password;
@@ -50,44 +40,74 @@ router.post('/', function(req, res, next) {
   client_keystore = lightwallet.keystore.deserialize(req.body.keystore);
   var gasPrice = parseFloat(gasPriceInGwei)*1.0e9;
   console.log(gasPrice);
-  
-  
   keystore = client_keystore;
-  genAddress(password);
-  setWeb3Provider(keystore);
-  console.log('provider is set')
-  var strPK;
-  var privateKey;
-  keystore.keyFromPassword(password, function (err, pwDerivedKey) {
-    strPK = pwDerivedKey;
-    console.log('derived key: ' + strPK);
-    privateKey = keystore.exportPrivateKey(fromAddr, pwDerivedKey);
-    console.log('pk: ' + privateKey);
-    var bufferPK = new Buffer(privateKey, 'hex');
-    console.log('buffer pk' + bufferPK.toString());
-    gasPrice = (web3.utils.toHex(gasPrice.toString())).toString();
-    gas = (web3.utils.toHex(gas)).toString();
-    console.log(gas);
-    console.log(gasPrice);
-    var rawTx = {
-      nonce: '0x00',
-      gasPrice: gasPrice,
-      gasLimit: gas,
-      to: toAddr,
-      value: '0x00',
-      data: ''
-    }
-    var tx = new Tx(rawTx);
-    tx.sign(bufferPK);
-    var serializedTx = tx.serialize();
-    console.log(serializedTx.toString());
-    console.log('0x' + serializedTx.toString('hex'));
-    try {
-      web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex')).on('receipt', console.log);
-    } catch (err) {
-      console.log(err);
-    }
+  
+  
+  // genAddress(password);
+  console.log(password)
+  var numAddr = 1;
+  keystore.keyFromPassword(password, function(err, pwDerivedKey) {
+    console.log(password)
+    keystore.generateNewAddress(pwDerivedKey, numAddr);
+    var serverGenAddr = keystore.getAddresses();
+    console.log(serverGenAddr);
+  })
+  
+  
+  // setWeb3Provider(keystore);
+  var web3Provider = new HookedWeb3Provider({
+    host: "https://ropsten.infura.io/KTxDHirVMJjBc0oLkjR2",
+    transaction_signer: keystore
   });
+  web3.setProvider(web3Provider);
+  console.log('provider is set')
+  
+  
+  //get transaction count NONCE
+  web3.eth.getTransactionCount(fromAddr).then((currentNonce) => {
+    console.log('decimal nonce: ');
+    console.log(currentNonce);
+    var hexNonce = (web3.utils.toHex(currentNonce.toString())).toString();
+    console.log('hexNonce: ' + hexNonce);
+  
+    
+    var strPK;
+    var privateKey;
+    keystore.keyFromPassword(password, function (err, pwDerivedKey) {
+      strPK = pwDerivedKey;
+      console.log('derived key: ' + strPK);
+      privateKey = keystore.exportPrivateKey(fromAddr, pwDerivedKey);
+      console.log('pk: ' + privateKey);
+      var bufferPK = new Buffer(privateKey, 'hex');
+      console.log('buffer pk' + bufferPK.toString());
+      gasPrice = (web3.utils.toHex(gasPrice.toString())).toString();
+      gas = (web3.utils.toHex(gas)).toString();
+      var hexValue = (web3.utils.toHex(valueInWei.toString())).toString();
+      console.log(gas);
+      console.log(gasPrice);
+      var rawTx = {
+        nonce: hexNonce,
+        gasPrice: gasPrice,
+        gasLimit: gas,
+        to: toAddr,
+        value: hexValue,
+        data: ''
+      }
+      var tx = new Tx(rawTx);
+      tx.sign(bufferPK);
+      var serializedTx = tx.serialize();
+      console.log(serializedTx.toString());
+      console.log('0x' + serializedTx.toString('hex'));
+      try {
+        web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex')).on('receipt', console.log);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  });
+
+  
+
 });
 
 module.exports = router;
